fbe75cddfad1e328cfd655ef7e3eadba7b59fe78
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-02-03 17:50:56 -0300

    refactor(packages): removing the design system package

--- packages/ui/src/contexts/index.ts
+++
@@ -1,1 +0,0 @@
-//Index created to export everything from @/components/contexts
--- packages/ui/src/index.ts
+++
@@ -1,4 +0,0 @@
-// Exports everything
-
-export { Button, buttonVariants, type ButtonProps } from "@/components/button";
-export { Input } from "@/components/input";
