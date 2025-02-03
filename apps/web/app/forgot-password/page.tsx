697d035b4eef2f1b3eb3d784ac1ebe5a2f650e50
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-02-03 18:05:58 -0300

    feat(login): prototype of the first screen

---
+++ apps/web/app/forgot-password/page.tsx
@@ -0,0 +1,4 @@
+interface ForgotPasswordPageProps {}
+export default function ForgotPasswordPage({}: ForgotPasswordPageProps) {
+  return <div>Not implemented yet</div>;
+}
---
+++ apps/web/app/register/page.tsx
@@ -0,0 +1,4 @@
+interface RegisterPageProps {}
+export default function RegisterPage({}: RegisterPageProps) {
+  return <div>Not implemented yet</div>;
+}
