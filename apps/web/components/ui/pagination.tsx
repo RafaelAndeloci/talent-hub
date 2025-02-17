e1d60fb2acc6fc29be60865f369933aac9b98fba
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-02-17 20:32:22 -0300

    fix(pagination): type error

--- apps/web/components/ui/pagination.tsx
+++ apps/web/components/ui/pagination.tsx
@@ -68,7 +68,7 @@ const PaginationPrevious = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to previous page"
-    size="default"
+    size="icon"
     className={cn("gap-1 pl-2.5", className)}
     {...props}
   >
@@ -84,7 +84,7 @@ const PaginationNext = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to next page"
-    size="default"
+    size="icon"
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
