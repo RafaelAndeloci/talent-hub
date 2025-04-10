bf6a9b8f58f3e8c081279889e92c6733b8eb40d8
Rafael Andeloci <rafael.goncalves@bkbank.com.br>
2025-04-10 19:06:40 -0300

    fix: just small things

--- apps/web/components/app/register/candidate/professional-info.tsx
+++ apps/web/components/app/register/candidate/professional-info.tsx
@@ -1,5 +1,22 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { CardContent, CardFooter } from "@/components/ui/card";
+import { useRegisterCandidate } from "@/contexts/register/candidate";
+
 export function ProfessionalInfoForm() {
-  return <div>ProfessionalInfo</div>;
+  const { goToStep } = useRegisterCandidate();
+
+  return (
+    <>
+      <CardContent className="flex flex-1"></CardContent>
+      <CardFooter className="flex w-full items-center justify-between gap-2">
+        <Button variant="ghost" onClick={() => goToStep("personal-info")}>
+          Voltar
+        </Button>
+
+        <Button onClick={() => alert("calma calabreso")}>Continuar</Button>
+      </CardFooter>
+    </>
+  );
 }
