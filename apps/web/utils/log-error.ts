8bb62930d3d1e6edba45ed69ca80ecd83eced47b
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-05-16 11:08:48 -0300

    ermmm

--- apps/web/utils/log-error.ts
+++ apps/web/utils/log-error.ts
@@ -1,5 +1,3 @@
 export function logError(err: any) {
-  // Do other things here maybe use some log system
-
-  console.error(JSON.stringify(err, null, 2));
+  console.error(JSON.stringify(err, null, 2))
 }
