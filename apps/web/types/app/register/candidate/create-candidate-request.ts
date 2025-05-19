c8f15563defb5e0f7fce83bcd305b2fedacd5c46
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-05-19 19:23:35 -0300

    feat(types): creating the register candidate request type

--- apps/web/types/app/register/candidate/create-candidate-request.ts
+++ apps/web/types/app/register/candidate/create-candidate-request.ts
@@ -1,5 +1,8 @@
-import { z } from "zod";
-export const createCandidateRequestSchema = z.object({});
+import { CandidateSchema } from '@talent-hub/shared'
+import { z } from 'zod'
+
+export const createCandidateRequestSchema = CandidateSchema
+
 export type CreateCandidateRequest = z.infer<
   typeof createCandidateRequestSchema
->;
+>
