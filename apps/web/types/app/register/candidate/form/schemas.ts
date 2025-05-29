79810b1df06c241a27fad90bcbd57155378216ba
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-05-28 21:17:24 -0300

    feat(types): need to add thees types because the shared package is broken .-.

--- apps/web/types/app/register/candidate/form/schemas.ts
+++ apps/web/types/app/register/candidate/form/schemas.ts
@@ -1,3 +1,4 @@
+import { Uf } from '@talent-hub/shared/types/uf'
 import { z } from 'zod'
 
 export const personalInfoSchema = z.object({
@@ -35,7 +36,39 @@ export const personalInfoSchema = z.object({
     .optional(),
 })
 
-// Step 2: Preferences Schema
+// Step 2: Contact Schema
+export const contactAddressSchema = z.object({
+  contact: z.object({
+    phone: z
+      .string({ message: 'Número de telefone deve ser informado' })
+      .min(10, 'Número de telefone deve ter no mínimo 10 caracteres.'),
+    email: z
+      .string({ message: 'E-mail para contato deve ser informado' })
+      .email({ message: 'Por favor forneça um e-mail válido' }),
+  }),
+
+  address: z.object({
+    number: z
+      .number({ message: 'Por favor forneça o número do endereço' })
+      .min(1, { message: 'Número da rua deve ter no mínimo 1 digito' }),
+    street: z
+      .string({ message: 'Rua deve ser informada' })
+      .min(5, { message: 'Nome da rua deve ter no mínimo 5 caracteres' }),
+    complement: z.string().optional(),
+    neighborhood: z
+      .string({ message: 'Bairro deve ser informado' })
+      .min(3, { message: 'Nome do bairro deve ter no mínimo 3 caracteres' }),
+    city: z
+      .string({ message: 'Cidade deve ser informada' })
+      .min(3, { message: 'Cidade deve ter no mínimo 3 caracteres.' }),
+    uf: z.nativeEnum(Uf, { message: 'Estado deve ser informado' }),
+    zipCode: z
+      .string({ message: 'CEP deve ser informado' })
+      .min(8, { message: 'CEP deve ter no mínimo 8 caracteres' }),
+  }),
+})
+
+// Step 3: Preferences Schema
 export const preferencesSchema = z.object({
   salary: z.string().optional(),
   employmentRegime: z.string().optional(),
@@ -45,7 +78,7 @@ export const preferencesSchema = z.object({
   benefits: z.array(z.string()).optional(),
 })
 
-// Step 3: Academic Schema
+// Step 4: Academic Schema
 export const academicSchema = z.object({
   courseName: z
     .string({ message: 'Nome do curso deve ser informado' })
@@ -53,14 +86,18 @@ export const academicSchema = z.object({
   institutionName: z
     .string({ message: 'Nome da instituição deve ser informada' })
     .min(2, 'Nome da instituição deve ter no mínimo 2 caracteres'),
-  degreeType: z.string().min(1, 'Por favor selecione uma opção'),
-  startDate: z.string().min(1, 'Data de inicio deve ser informada'),
+  degreeType: z
+    .string({ message: 'Por favor selecione uma opção' })
+    .min(1, 'Por favor selecione uma opção'),
+  startDate: z
+    .string({ message: 'Data de início deve ser informada' })
+    .min(1, 'Data de inicio deve ser informada'),
   endDate: z.string().optional(),
   isCurrent: z.boolean().optional(),
   gradePointAverage: z.string().optional(),
 })
 
-// Step 4: Professional Schema
+// Step 5: Professional Schema
 export const professionalSchema = z.object({
   position: z
     .string({ message: 'Posição deve ser informada' })
@@ -74,11 +111,15 @@ export const professionalSchema = z.object({
     .min(1, 'Data de inicio deve ser informada'),
   endDate: z.string().optional(),
   isCurrent: z.boolean().optional(),
-  employmentType: z.string().min(1, 'Por favor selecione uma opção'),
-  workplaceType: z.string().min(1, 'Por favor selecione uma opção'),
+  employmentType: z
+    .string({ message: 'Por favor selecione uma opção' })
+    .min(1, 'Por favor selecione uma opção'),
+  workplaceType: z
+    .string({ message: 'Por favor selecione uma opção' })
+    .min(1, 'Por favor selecione uma opção'),
 })
 
-// Step 5: Languages and Skills Schema
+// Step 6: Languages and Skills Schema
 export const languagesAndSkillsSchema = z.object({
   languages: z.array(
     z.object({
@@ -101,12 +142,14 @@ export const languagesAndSkillsSchema = z.object({
 // Combined schema for all steps
 export const candidateFormSchema = z.object({
   personalInfo: personalInfoSchema,
+  contactAddress: contactAddressSchema,
   preferences: preferencesSchema.optional(),
   academic: academicSchema,
-  professional: professionalSchema,
+  professional: professionalSchema.optional(),
   languagesAndSkills: languagesAndSkillsSchema,
 })
 export type PersonalStepSchema = z.infer<typeof personalInfoSchema>
+export type ContactAddressStepSchema = z.infer<typeof contactAddressSchema>
 export type PreferencesStepSchema = z.infer<typeof preferencesSchema>
 export type AcademicStepSchema = z.infer<typeof academicSchema>
 export type ProfessionalStepSchema = z.infer<typeof professionalSchema>
